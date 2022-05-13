985788a29d6aa4be48e30f0f7392f139a2b79c0c
latifasalsab <72429520+latifasalsab@users.noreply.github.com>
2022-05-13 14:08:04 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+while (true) {
+  var number, group;
+  number = prompt("enter numbers to calculate ").replace(
+    /[^0-9%^*\/()\-+.]/g,
+    ""
+  );
+
+  group = number.split(" ");
+  if ((Number.parseInt(group[0]) | Number.parseInt(group[2])) > 1000000) {
+    alert("Maximum number input is 1 million");
+  } else {
+    alert(eval(number));
+  }
+}
